30950f8be4e46660ed128aa233ec11ea22494ff2
Bharath Saravanan <bharathkumar.saravanan@customerlabs.co>
2024-10-23 23:03:48 +0530

    #kan-51 initial project setup #time 1h #comment project setuped and ran in mobile device. And the changes i making updating in the mobile. #done

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Now its makes sense man. Oh fuck its fucking terrible. and its awesome</Text>
+    </View>
+  );
+}
